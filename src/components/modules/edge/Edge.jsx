f8d4174466241c3385a247dbe81f1acea128aef8
Demyus <orlando.agundez.ramirez@gmail.comm>
2022-12-04 18:44:09 -0800

    adding edge module

--- src/components/modules/edge/Edge.jsx
+++ src/components/modules/edge/Edge.jsx
@@ -1,14 +1,31 @@
 import Card from "../../ui/Card";
 import classes from "../conditions/Conditions.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { charStatActions } from "../../../store/stats-slice";
 
-const Edge = (props) => {
+const Edge = (props) => {  
+  let edge = [];
+  let edgeUsed = []
+  const stat = useSelector(state => state.charStats );
+  const dispatch = useDispatch();
+
+  const useEdge = () => {
+    dispatch(charStatActions.addStat('usedEdge'))
+  }
+
+  const recoverEdge = () => {        
+    dispatch(charStatActions.removeStat('usedEdge'))
+  } 
+
+  for (let x = 0; x < (stat.Edge - stat.usedEdge); x++) { edge.push(<button className={classes.btnNone}  onClick={useEdge} >🔴</button>) }
+  for (let z = 0; z < stat.usedEdge; z++)    { edgeUsed.push(<button className={classes.btnNone} onClick={recoverEdge} >⚪</button>) }
+
   return (
     <Card>
       <div className="col text-center">
         <strong>EDGE:</strong> <br />
-        <button className={classes.btnNone} click="dmgtoggle">🔴</button>
-        <button className={classes.btnNone} click="dmgtoggle">⚪</button>
-        <span if="parseInt(edg) == 0">No Edge</span>
+        {edge}{edgeUsed}
+        { !stat.Edge && <span>No Edge</span>}
       </div>
     </Card>
   );
